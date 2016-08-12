da759bf3ab071f699418db87721207ef7f49644c
Ashwin balakrishnan <nadarashwin@gmail.com>
2016-08-12 12:18:58 +0530

    tuned the script so that the password prompt will be once

--- mysql_python_query.py
+++ mysql_python_query.py
@@ -12,38 +12,45 @@ else:
         _host = sys.argv[1]
         _user = sys.argv[2]
         _db   = sys.argv[3]
+
+        _pass = getpass.getpass("please enter password:- ") ##password wont be visible
+        db = MySQLdb.connect(host = _host, user = _user,db =  _db, passwd = _pass )
         while True:
                 cham = raw_input('''
-                                please enter the command to be executed:- 
-                                just punch in 'quit' to exit the activity:-
-                                ''')
-        #        _pass = raw_input("please enter password:- ")
-                
+                        please enter the command to be executed:- 
+                        to drop a single table punch in 'drop table' and next prompt will ask for table name
+                        to drop all tables punch in 'drop table all'
+                        just punch in 'quit' to exit the activity:-
+                        ''')
+                if cham.lower() == "quit":
+                        sys.exit()
 
-                if cham == "drop table":
-                        _pass = getpass.getpass("please enter password:- ") ##password wont be visible
-                	db = MySQLdb.connect(host = _host, user = _user,db =  _db, passwd = _pass )
+                if cham.lower() == "drop table all":
                 	cursor = db.cursor()
                 	cursor.execute("show tables")
                 	for i in cursor.fetchall():
                 		try:
                 			cursor.execute("drop table" + " " + (i[0]))
-                			#print cursor.fetchall()
                 		except:
                 			cursor.execute("SET FOREIGN_KEY_CHECKS=0")
                 			cursor.execute("drop table" + " " + (i[0]))
                 			cursor.execute("SET FOREIGN_KEY_CHECKS=1")
-        #        		print "all the tables has been deleted"
-                	db.close()
-                elif cham.lower()  == "quit":
-                        sys.exit()
+                		print "all the tables has been deleted"
+                elif cham.lower() == "drop table":
+                        _ed = raw_input("please enter the table name:- ")
+                        cursor = db.cursor()
+                        try:
+                                cursor.execute("drop table" + " " + _ed)
+                        except:
+                                cursor.execute("SET FOREIGN_KEY_CHECKS=0")
+                                cursor.execute("drop table" + " " + _ed)
+                                cursor.execute("SET FOREIGN_KEY_CHECKS=1")
+                        print "Table " + _ed + " has been deleted"
+
                 else:
-                        _pass = getpass.getpass("please enter password:- ") ##password wont be visible
-                	db = MySQLdb.connect(host = _host, user = _user,db =  _db, passwd = _pass )
                 	cursor = db.cursor()
                 	cursor.execute(cham)
                 	print cursor.fetchall()
-                	db.close()
 
 
 
